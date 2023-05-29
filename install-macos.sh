c5a5c49799b1dacf6a01427110d4cc90f63cc374
shahzc <133967890+shahzc@users.noreply.github.com>
2023-05-29 17:20:32 -0400

    Update install-macos.sh

--- install-macos.sh
+++ install-macos.sh
@@ -60,13 +60,13 @@ set_defaults() {
 		defaults write $app WebKitPreferences.developerExtrasEnabled -bool YES
 		defaults write $app WebKitDeveloperExtrasEnabledPreferenceKey -bool YES
 		defaults write $app WebKitPreferences.javaScriptCanOpenWindowsAutomatically -bool NO
-
-		{ set +x; } 2> /dev/null
+		{ set +x; } 2>/dev/null
 	done
+	
 	for app in com.apple.Safari.SandboxBroker com.apple.SafariTechnologyPreview.SandboxBroker; do
 		set -x
 		defaults write $app ShowDevelopMenu -bool YES
-		{ set +x; } 2> /dev/null
+		{ set +x; } 2>/dev/null
 	done
 	set -x
 	
@@ -86,18 +86,29 @@ set_defaults() {
 	defaults write com.apple.DiskUtility WorkspaceShowAPFSSnapshots -bool YES
 	killall Disk\ Utility 2> /dev/null
 
-
+	
 	# Xcode
-	defaults write com.apple.dt.Xcode ShowDVTDebugMenu -bool YES
-	defaults write com.apple.dt.Xcode DVTTextIndentUsingTabs -bool YES
-	defaults write com.apple.dt.Xcode IDEFileExtensionDisplayMode -int 1
-	defaults write com.apple.dt.Xcode DVTTextShowFoldingSidebar -bool YES
-	defaults write com.apple.dt.Xcode IDEIssueNavigatorDetailLevel -int 30
-	defaults write com.apple.dt.Xcode IDESearchNavigatorDetailLevel -int 30
-	defaults write com.apple.dt.Xcode DVTEnableDockIconVersionNumber -bool YES
-	defaults write com.apple.dt.Xcode IDEAlwaysShowCompressedStackFrames -bool NO
-	defaults write com.apple.dt.Xcode IDEIndexerActivityShowNumericProgress -bool YES
-	killall Xcode 2> /dev/null
+	echo "Setting up Xcode.app"
+	if [ -d "/Applications/Xcode.app" ]; then
+    	    xcode-select -s /Applications/Xcode.app
+    	    xcodebuild -license accept
+    	    xcodebuild -runFirstLaunch
+    	    mkdir -p /Library/Developer/
+    	    ln -sf /Applications/Xcode.app/Contents/version.plist /Library/Developer/version.plist
+	    defaults write com.apple.dt.Xcode ShowDVTDebugMenu -bool YES
+	    defaults write com.apple.dt.Xcode DVTTextIndentUsingTabs -bool YES
+	    defaults write com.apple.dt.Xcode IDEFileExtensionDisplayMode -int 1
+	    defaults write com.apple.dt.Xcode DVTTextShowFoldingSidebar -bool YES
+	    defaults write com.apple.dt.Xcode IDEIssueNavigatorDetailLevel -int 30
+	    defaults write com.apple.dt.Xcode IDESearchNavigatorDetailLevel -int 30
+	    defaults write com.apple.dt.Xcode DVTEnableDockIconVersionNumber -bool YES
+	    defaults write com.apple.dt.Xcode IDEAlwaysShowCompressedStackFrames -bool NO
+	    defaults write com.apple.dt.Xcode IDEIndexerActivityShowNumericProgress -bool YES
+	    killall Xcode 2> /dev/null
+	else
+    	    echo "Xcode.app not found in /Applications"
+	fi
+	
 	
 
 	# Global
@@ -175,5 +186,4 @@ set_defaults() {
 	{ set +x; } 2> /dev/null
 	
 }
-
 set_defaults 
