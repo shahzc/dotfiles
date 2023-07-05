c3389bb5164079ea73c38389de8cc16052c88c3a
shahzc <133967890+shahzc@users.noreply.github.com>
2023-07-05 08:26:47 -0400

    Add files via upload

---
+++ homebrew-setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+		
+	set -x
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install autoconf
+ brew install automake
+  brew install bash
+   brew install brotli
+    brew install c-ares
+     brew install ca-certificates
+      brew install ccat
+       brew install cmake
+	brew install coreutils
+	 brew install gettext
+	  brew install icu4c
+           brew install libidn2
+            brew install libnghttp2
+             brew install libtool
+              brew install libunistring
+               brew install libuv
+                brew install make
+               brew install ncurses
+              brew install node
+             brew install openssl@1.1
+            brew install openssl@3
+           brew install pcre
+          brew install pcre2
+         brew install ripgrep
+        brew install the_silver_searcher
+       brew install tree
+      brew install wget
+     brew install node
+    brew install zsh
+   brew install bash
+  brew install ios-deploy
+ brew install socat
+brew install p7zip
+
+	{ set +x; } 2>/dev/null
+#https://opensource.apple.com/tarballs/Security/
